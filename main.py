24d0a12de3e099e5a51fe5e89680e2fb9dd4946f
barve-sneha26 <barve.sneha26@outlook.com>
2024-12-01 19:39:37 -0500

    Data validation done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.winequalitypred import logger 
-from src.winequalitypred.pipeline.data_ingestion_pipeline import DataIngestionPipeline 
+from src.winequalitypred.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+from src.winequalitypred.pipeline.data_validation_pipeline import DataValidationPipeline  
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -11,3 +12,12 @@ except Exception as e:
         logger.exception(e)
         raise e 
 
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationPipeline()
+   data_ingestion.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e 
