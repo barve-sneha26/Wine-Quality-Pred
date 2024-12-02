3a865347ba3a07024a8d1ae248130872039003c7
barve-sneha26 <barve.sneha26@outlook.com>
2024-12-02 10:49:47 -0500

    model training

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.winequalitypred import logger
 from src.winequalitypred.pipeline.data_ingestion_pipeline import DataIngestionPipeline
 from src.winequalitypred.pipeline.data_validation_pipeline import DataValidationPipeline  
 from src.winequalitypred.pipeline.data_transformation_pipeline import DataTransformationPipeline 
+from src.winequalitypred.pipeline.model_trainer_pipeline import ModelTrainerPipeline 
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -34,3 +35,12 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerPipeline()
+   data_ingestion.initiate_model_training()
+   logger.info(f">>>>>> Stage: {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e 
