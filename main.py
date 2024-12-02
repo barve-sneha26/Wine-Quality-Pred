d43b05733b318b55f4c51c1a02b1a9212f726d70
barve-sneha26 <barve.sneha26@outlook.com>
2024-12-01 19:22:01 -0500

    Data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.winequalitypred import logger 
+from src.winequalitypred.pipeline.data_ingestion_pipeline import DataIngestionPipeline 
 
-logger.info("Custom logging") 
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e 
+
